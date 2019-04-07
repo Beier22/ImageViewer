47b0ea15475843d8609e3515448a82c5ba840ba9
PLKING <sz.soczewka@gmail.com>
2019-04-07 15:14:10 +0200

    h

--- src/imageviewerproject/FXMLDocumentController.java
+++ src/imageviewerproject/FXMLDocumentController.java
@@ -6,6 +6,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -17,8 +19,8 @@ import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
 import javafx.stage.Stage;
 
-public class FXMLDocumentController implements Initializable
-{
+public class FXMLDocumentController implements Initializable {
+
     private final List<Image> images = new ArrayList<>();
     private int currentImageIndex = 0;
 
@@ -37,81 +39,106 @@ public class FXMLDocumentController implements Initializable
     @FXML
     private ImageView imageView;
 
-    private void handleBtnLoadAction(ActionEvent event)
-    {
+    private boolean running;
+    private Thread t;
+
+    private void handleBtnLoadAction(ActionEvent event) {
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Select image files");
-        fileChooser.getExtensionFilters().add(new ExtensionFilter("Images", 
-            "*.png", "*.jpg", "*.gif", "*.tif", "*.bmp", "*.jpeg"));        
+        fileChooser.getExtensionFilters().add(new ExtensionFilter("Images",
+                "*.png", "*.jpg", "*.gif", "*.tif", "*.bmp", "*.jpeg"));
         List<File> files = fileChooser.showOpenMultipleDialog(new Stage());
-                
-        if (!files.isEmpty())
-        {
-            files.forEach((File f) ->
-            {
+
+        if (!files.isEmpty()) {
+            files.forEach((File f)
+                    -> {
                 images.add(new Image(f.toURI().toString()));
             });
             displayImage();
         }
     }
 
-    private void handleBtnPreviousAction(ActionEvent event)
-    {
-        if (!images.isEmpty())
-        {
-            currentImageIndex = 
-                    (currentImageIndex - 1 + images.size()) % images.size();
+    private void handleBtnPreviousAction(ActionEvent event) {
+        if (!images.isEmpty()) {
+            currentImageIndex
+                    = (currentImageIndex - 1 + images.size()) % images.size();
             displayImage();
         }
     }
 
-    private void handleBtnNextAction(ActionEvent event)
-    {
-        if (!images.isEmpty())
-        {
+    private void handleBtnNextAction(ActionEvent event) {
+        if (!images.isEmpty()) {
             currentImageIndex = (currentImageIndex + 1) % images.size();
             displayImage();
         }
     }
 
-    private void displayImage()
-    {
-        if (!images.isEmpty())
-        {
+    private void displayImage() {
+        if (!images.isEmpty()) {
             imageView.setImage(images.get(currentImageIndex));
         }
     }
 
     @Override
-    public void initialize(URL url, ResourceBundle rb)
-    {
-        btnLoad.setOnAction((ActionEvent event) ->
-        {
+    public void initialize(URL url, ResourceBundle rb) {
+        running = false;
+
+        btnLoad.setOnAction((ActionEvent event)
+                -> {
             handleBtnLoadAction(event);
         });
 
-        btnPrevious.setOnAction((ActionEvent event) ->
-        {
+        btnPrevious.setOnAction((ActionEvent event)
+                -> {
             handleBtnPreviousAction(event);
         });
-        
-        btnNext.setOnAction((ActionEvent event) ->
-        {
+
+        btnNext.setOnAction((ActionEvent event)
+                -> {
             handleBtnNextAction(event);
         });
-        
+
         File folder = new File("src\\imageviewerproject\\gui\\images");
-        
+
         List<File> files = Arrays.asList(folder.listFiles());
-                
-        if (!files.isEmpty())
-        {
-            files.forEach((File f) ->
-            {
+
+        if (!files.isEmpty()) {
+            files.forEach((File f)
+                    -> {
                 images.add(new Image(f.toURI().toString()));
             });
             displayImage();
         }
+    }
+
+    @FXML
+    private void startSlideshow(ActionEvent event) {
+        running = true;
+
+      
+            t = new Thread(() -> {
+                while (running) {
+                    try {
+                        Thread.sleep(2000);
+                        btnNext.fire();
+                        System.out.println("Next image shown");
+
+                    } catch (InterruptedException ex) {
+                        Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+
+                }
+                System.out.println("Thread working still");
+            }
+            );
+            t.start();
+        
+
+    }
+
+    @FXML
+    private void stopSlideshow(ActionEvent event) {
+        running = false;
     }
 
 }
